817586441aabe96096e5de248cc6ea8e709dc888
TechGriffo254 <griffotech254@gmail.com>
2023-06-15 05:22:51 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line with puts funtion
+ * Return: 0 (bravo)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
