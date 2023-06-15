9bc365eedf57dd7b9413c749e9ac29b5eb2df885
TechGriffo254 <griffotech254@gmail.com>
2023-06-15 10:01:21 -0700

    last code

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+/**
+ * main - A program that prints a line with puts funtion
+ * Return: 0 (bravo)
+ */
+int main(void)
+{
+	write(2, "nd that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
