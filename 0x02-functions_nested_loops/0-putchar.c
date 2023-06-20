8f62a425b4d6d52375a5f91c7c2162bfdf308927
TechGriffo254 <griffotech254@gmail.com>
2023-06-20 03:17:15 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,18 @@
 #include "main.h"
 /**
- * main - will print _putchar, followed by new line 
- * Return: 0 (Success), 1 (failure)
- */
+* main - will print _putchar, followed by new line
+* Return: 0 (Success), 1 (failure)
+*/
 int main(void)
 {
-	char grif[] = "_putchar";
+char grif[] = "_putchar";
 
-	int c;
+int c;
 
-	for (c = 0; c < 8; c++)
-	{
-		_putchar(grif[c]);
-	}
-	_putchar('\n');
-	return (0);
+for (c = 0; c < 8; c++)
+{
+_putchar(grif[c]);
+}
+_putchar('\n');
+return (0);
 }
