62af93ed46686cd6058b94fb7440ceab756f9c9c
TechGriffo254 <griffotech254@gmail.com>
2023-06-20 03:08:57 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,24 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ *
+ * Desc: Header file is  containing declarations for all functions
+ */
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void time_teble(void);
+int add(int, int);
+void print_to_98(int n);
+void print_time_table(int n);
+
+#endif
