fb9309c1f056ba7ef061ae10fda6d08ba5444348
TechGriffo254 <griffotech254@gmail.com>
2023-06-22 00:28:14 -0700

    task 0

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+/**
+ * This is the header file that stores all the protypes
+ */
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+#endif
