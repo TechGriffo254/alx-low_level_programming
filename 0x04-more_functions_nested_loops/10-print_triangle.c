55e7e9187d6cb070779e195bc4b728ed655a542d
TechGriffo254 <griffotech254@gmail.com>
2023-06-22 05:30:37 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -2,7 +2,7 @@
 /**
  * print_triangle - prints a triangle, followed by a new line
  * @size: size of the triangle given
- * Return: nothing because of void prototype 
+ * Return: nothing because of void
  */
 void print_triangle(int size)
 {
