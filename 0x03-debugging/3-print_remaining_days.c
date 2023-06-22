2efb28c6ecbcac4dab04d3143346d2fdfa587a49
TechGriffo254 <griffotech254@gmail.com>
2023-06-22 01:15:33 -0700

    last task

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -3,7 +3,7 @@
 
 /**
 * print_remaining_days - takes a date and prints how many days are
-* left in the year, taking leap years into account and saving 
+* left in the year, taking leap years into account and saving
 * @month: month in number format
 * @day: day of the month
 * @year: year
