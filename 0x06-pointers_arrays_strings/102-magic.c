fe978d9f7ed5f62e7e8b83de6487c042109b11f3
TechGriffo254 <griffotech254@gmail.com>
2023-06-29 06:12:03 -0700

    102-magic.c

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int n;
+	int a[5];
+	int *p;
+
+	a[2] = 1024;
+	p = &n;
+	/*
+   * write your line of code here...
+   */
+  *(p + 5) = 98;
+	/* ...so that this prints 98\n */
+	printf("a[2] = %d\n", a[2]);
+	return (0);
+}
