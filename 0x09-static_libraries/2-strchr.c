2e0967818cf5eea401b789e43d45551629baa3f2
TechGriffo254 <griffotech254@gmail.com>
2023-07-07 07:38:48 -0700

    0. A library is not a luxury but one of the necessities of life

--- 0x09-static_libraries/2-strchr.c
+++ 0x09-static_libraries/2-strchr.c
@@ -2,25 +2,27 @@
 #include <stdio.h>
 
 /**
- * *_strchr - locates character in a string
- * @s: string to search
- * @c: char to be find
- *
- * Return: a pointer to the first occurrence
- */
+* *_strchr - locates character in a string
+* @s: string to search
+* @c: char to be find
+*
+* Return: a pointer to the first occurrence
+*/
 char *_strchr(char *s, char c)
 {
-		int k;
+int k;
 
-		while (1)
-		{
-			k = *s++;
-			if (k == c)
-			{
-				return (s - 1);
-			}
-			if (k == 0)
-			{
-				return (NULL);
-			}
-		}
+while (1)
+{
+k = *s++;
+if (k == c)
+{
+return (s - 1);
+}
+if (k == 0)
+{
+return (NULL);
+}
+
+}
+}
