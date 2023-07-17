941bc83d9fa0fb591f8c4313e8c697d619ade897
TechGriffo254 <griffotech254@gmail.com>
2023-07-17 05:16:16 -0700

    5-free_dog.c

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -2,17 +2,17 @@
 #include "dog.h"
 
 /**
-* free_dog - ...
-* @d: ...
-*
-* Return: ...
-*/
+ * free_dog - ...
+ * @d: ...
+ *
+ * Return: ...
+ */
 void free_dog(dog_t *d)
 {
-if (d != NULL)
-{
-free(d->owner);
-free(d->name);
-free(d);
-}
+	if (d != NULL)
+	{
+		free(d->owner);
+		free(d->name);
+		free(d);
+	}
 }
